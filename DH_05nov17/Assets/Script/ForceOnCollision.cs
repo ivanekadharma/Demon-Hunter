a0bd5edf47fbd3dc3c02488c218ffad3a19ab5bb
Alfi <33090806+veelyne@users.noreply.github.com>
2017-11-05 14:53:06 +0700

    05nov
    baru jadi segitu

---
+++ DH_05nov17/Assets/Script/ForceOnCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceOnCollision : MonoBehaviour {
+	public float forcePower = 2000;
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Player") {
+			Vector3 heading = col.transform.position - transform.position;
+
+			col.gameObject.GetComponent<Rigidbody> ().AddForce (heading * forcePower);
+		}
+	}
+}
---
+++ DH_05nov17/Assets/SvGenerateEnemies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SvGenerateEnemies : MonoBehaviour {
+	public Transform InstantiateMe;
+	public int totalEnemy = 50;
+	int temp;
+	float x; float y; float z;
+	void Start () {
+		for(int i = 0; i<=totalEnemy; i++){
+			x = Random.Range (-60, 55);
+			z = Random.Range (-55, 55);
+			y = 1.1f;
+			var go = Instantiate (InstantiateMe, transform.position = (new Vector3 (x, y, z)), Quaternion.identity);	
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
