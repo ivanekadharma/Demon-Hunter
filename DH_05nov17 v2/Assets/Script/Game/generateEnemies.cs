574423f23216edff3a876e8e33985143f68aa79d
Alfi <33090806+veelyne@users.noreply.github.com>
2017-11-05 17:43:59 +0700

    v2
    v2

---
+++ DH_05nov17 v2/Assets/Script/Game/generateEnemies.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateEnemies : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform InstantiateMe;
+	public int totalEnemy = 20;
+	float x; float y; float z;
+	void Start () {
+		for(int i = 0; i<=totalEnemy; i++){
+			x = Random.Range (-60, 55);
+			z = Random.Range (-55, 55);
+			y = 1;
+			var go = Instantiate (InstantiateMe, transform.position = (new Vector3 (x, y, z)), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DH_05nov17 v2/Assets/Script/Game/getScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getScore : MonoBehaviour {
+
+	public int score = 0;
+	int tempScore;
+	int totalEnemiesNow;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	} 
+
+
+	public void OnGUI(){
+		tempScore = score-1;
+		totalEnemiesNow = GetComponent<generateEnemies> ().totalEnemy;
+		if (tempScore == totalEnemiesNow) {
+			GUI.Label (new Rect(10, 10, 100, 20), "You Win !");
+		}else GUI.Label (new Rect(10, 10, 100, 20), tempScore + " / " + totalEnemiesNow + " Demons");
+	}
+}
